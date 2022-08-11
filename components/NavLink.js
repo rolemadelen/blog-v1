3b1485c8e49aab64c32fb5e56b05c7e77ad47a86
rolemadelen <rolemadelen17@pm.me>
2022-08-11 18:09:13 +0900

    overall UI modified

--- components/NavLink.js
+++ components/NavLink.js
@@ -4,7 +4,7 @@ const NavLink = (props) => {
   return (
     <Link href={props.link}>
       <a
-        className={`${props.customClass} text-gray-700 hover:no-underline hover:text-[#289aff] duration-300`}
+        className={`${props.customClass} text-gray-700 hover:no-underline duration-300`}
       >
         {props.value}
       </a>
