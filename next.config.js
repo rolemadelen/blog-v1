35d93a8b02555894fefd45991e63fdc57ebab813
Madelen Yoo <rolemadelen@tuta.io>
2022-12-21 09:30:04 +0900

    Reconstructing Home UI
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const { withContentlayer } = require('next-contentlayer')
 module.exports = withContentlayer()({
   reactStrictMode: true,
   eslint: {
-    dirs: ['pages', 'components', 'lib', 'layouts', 'data'],
-  },
+      dirs: ['pages', 'components', 'lib', 'layouts', 'data'],
+    },
 })
---
+++ pages/about.js
@@ -0,0 +1,33 @@
+import Container from '@components/Container';
+import Footer from '@components/Footer';
+import utilStyles from '@styles/utils.module.scss';
+import Link from 'next/link';
+
+export default function About() {
+	return (
+		<div className={`${utilStyles.hero} ${utilStyles.about}`}>
+			<p className={`${utilStyles.h1}`}>
+				Hi. I'm <b>Rolemadelen</b> or <b>Madelen</b> for short.
+			</p>
+			<p className={`${utilStyles.h2}`}>I'm a Frontend Developer.</p>
+			<div className={`${utilStyles.aboutMenu}`}>
+				<span>
+					<Link
+						key={'home'}
+						href={'/'}
+						passHref>
+						{'home'}
+					</Link>
+				</span>
+				<span>
+					<Link
+						key={'blog'}
+						href={'/blog/'}
+						passHref>
+						{'blog'}
+					</Link>
+				</span>
+			</div>
+		</div>
+	);
+}
