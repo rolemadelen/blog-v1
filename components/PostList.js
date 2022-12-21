19cfb0868d748378829fb1bd7683d3d7207014a6
Madelen Yoo <rolemadelen@tuta.io>
2022-12-21 22:24:08 +0900

    blog ui in progress
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- components/PostList.js
+++ components/PostList.js
@@ -1,49 +1,26 @@
 import Link from 'next/link';
-import { List, Title } from './custom-tw-components';
+import { List, Title, PostDate } from './custom-tw-components';
 
 const PostList = ({ posts }) => {
-  const m3 = new Date(); // new
-  const m7 = new Date(); // updated
-  m3.setDate(m3.getDate() - 3);
-  m7.setDate(m7.getDate() - 5);
-
-  const ymd3 =
-    m3.getUTCFullYear() +
-    '-' +
-    (m3.getUTCMonth() + 1) +
-    '-' +
-    (m3.getUTCDate() <= '9' ? '0' : '') +
-    m3.getUTCDate();
-  const ymd7 =
-    m7.getUTCFullYear() +
-    '-' +
-    (m7.getUTCMonth() + 1) +
-    '-' +
-    (m7.getUTCDate() <= '9' ? '0' : '') +
-    m7.getUTCDate();
-
-  return (
-    <>
-      {posts.map((post) => (
-        <Link
-          key={`${post.lang}-${post.slug}-$`}
-          href={`/blog/${post.lang}/${post.slug}`}
-          passHref>
-          <List>
-            <Title>
-              {post.title}
-              {post.date && post.date.split(' ')[0] >= ymd3 && (
-                <span className='newPost'>NEW</span>
-              )}
-              {post.updated && post.updated.split(' ')[0] >= ymd7 && (
-                <span className='updated'>updated</span>
-              )}
-            </Title>
-          </List>
-        </Link>
-      ))}
-    </>
-  );
+    return (
+        <>
+            {posts.map((post) => (
+                <Link
+                    key={`${post.lang}-${post.slug}-$`}
+                    href={`/blog/${post.lang}/${post.slug}`}
+                    passHref>
+                    <List>
+                        <PostDate>
+                            {post.date.split(' ')[0]}
+                        </PostDate>
+                        <Title>
+                            {post.title}
+                        </Title>
+                    </List>
+                </Link>
+            ))}
+        </>
+    );
 };
 
 export default PostList;
