7f4bda76be71f055c3ce0a55e8e746552bb4cd0d
Madelen Yoo <rolemadelen@tuta.io>
2022-12-22 22:06:25 +0900

    post list and post ui modified
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- data/navlink.js
+++ data/navlink.js
@@ -1,16 +1,9 @@
 const navlink = {
-  base: [
-    // { name: "/about", link: "/about" },
-    { name: 'Blog', link: '/blog' },
-    // { name: "/dsa", link: "/dsa/Graph/adjacency-list" },
-  ],
-  blog: [
-    { name: 'Data Structures', link: '/blog/ds' },
-    { name: 'Algorithms', link: '/blog/algo' },
-    { name: 'Programming', link: '/blog/programming' },
-    { name: 'CP', link: '/blog/cp' },
-    { name: 'Log', link: '/blog/log' },
-  ],
+	base: [{ name: 'Blog', link: '/blog' }],
+	blog: [
+		{ name: 'Data Structures', link: '/blog/dsa' },
+		{ name: 'Log', link: '/blog/note' },
+	],
 };
 
 export default navlink;
