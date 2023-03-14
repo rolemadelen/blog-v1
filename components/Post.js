65283081c39118d6ef40e002ac3ad2175c6bf0de
rolemadelen <rolemadelen17@pm.me>
2023-03-14 14:42:06 -0400

    daisy UI applied

--- components/Post.js
+++ components/Post.js
@@ -1,36 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Date from '@components/Date';
 import {
-	PostContainer,
-	PageSecondTitle,
-	BackButton,
-	HeaderContainer,
+    PostContainer,
+    BackButton,
+    HeaderContainer,
 } from './custom-tw-components';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import metadata from '@data/metadata';
 
-const Post = ({ post, children }) => {
-	const router = useRouter();
-	router.basePath = '/blog';
 
-	return (
-		<PostContainer>
-			<Head>
-				<title>{post.title}</title>
-			</Head>
-			<HeaderContainer>{post.posttitle}</HeaderContainer>
-			<div className={'flex px-4'}>
-				<PageSecondTitle>{`by ${metadata.author.id}`}</PageSecondTitle>
-				<Date
-					updated={post.updated}
-					dateString={post.updated ? post.updated : post.date}
-					customClass={'mt-2 mb-2 text-md break-normal inline-block text-right'}
-				/>
-			</div>
-			<BackButton onClick={() => router.push('/blog')}>{'←'}</BackButton>
-			{children}
-		</PostContainer>
-	);
+const Post = ({ post, children }) => {
+    Post.propTypes = {
+        post: PropTypes.object.isRequired,
+        children: PropTypes.node.isRequired,
+    };
+    const router = useRouter();
+    router.basePath = '/blog';
+
+    return (
+        <PostContainer>
+            <Head>
+                <title>{post.title}</title>
+            </Head>
+            <HeaderContainer>{post.posttitle}</HeaderContainer>
+            <div className={'flex'}>
+                <div className={'font-["AmazonEmberDisplay"] text-lg my-1 w-full text-primary'}>{`by ${metadata.author.id}`}</div>
+                <Date
+                    updated={post.updated}
+                    dateString={post.updated ? post.updated : post.date}
+                    customClass={'mt-2 mb-2 text-md break-normal inline-block text-right'}
+                />
+            </div>
+            <BackButton onClick={() => router.push('/blog')}>{'←'}</BackButton>
+            {children}
+        </PostContainer>
+    );
 };
 
 export default Post;
--- layouts/blogpost.js
+++ layouts/blogpost.js
@@ -1,23 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Post from '@components/Post';
 import Footer from '@components/Footer';
 import Comments from '@components/Comments';
 import Preview from '@lib/codeblock.js';
 import {
-	Article,
-	PostListContainer,
+    Article,
+    PostListContainer,
 } from '@components/custom-tw-components';
 
+
 const BlogPostLayout = ({ post }) => {
-	return (
-		<PostListContainer>
-			<Post post={post} />
-			<Article>
-				<Preview markdown={post.markdown} />
-			</Article>
-			<Comments />
-			<Footer />
-		</PostListContainer>
-	);
+    BlogPostLayout.propTypes = {
+        post: PropTypes.object.isRequired
+    };
+    return (
+        <PostListContainer>
+            <Post post={post} />
+            <Article>
+                <Preview markdown={post.markdown} />
+            </Article>
+            <Comments />
+            <Footer />
+        </PostListContainer>
+    );
 };
 
 export default BlogPostLayout;
