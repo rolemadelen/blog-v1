697fe34e1044a2e79f5a06a73791542b1b027215
Madelen Yoo <rolemadelen@tuta.io>
2023-03-14 14:48:17 -0400

    eslint fixed
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import '@styles/global.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
