39485ac9ac54c5252a3bdf5d994e207c5e14991f
Madelen Yoo <rolemadelen@tuta.io>
2023-04-02 15:58:39 -0400

    About page - english added
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- pages/about.js
+++ pages/about.js
@@ -33,10 +33,17 @@ export default function About() {
             </div>
             <div className='text-primary'>
                 <p>
-				안녕하세요. Jii Eu(지이) 입니다. 태고(太鼓)치는 취미를 가지고 있지만 태고의 달인은 잘 못합니다. 켄터키주 조용한 곳에 파트너와 강아지 한 마리와 함께 살고 있습니다 👸🏻🐾 
+				안녕하세요. Jii Eu(지이) 입니다. 태고(太鼓)치는 취미를 가지고 있지만 <a href='https://taiko-ch.net/index.php'>태고의 달인</a>은 잘 못합니다. 켄터키주 조용한 곳에 파트너와 강아지 한 마리와 함께 살고 있습니다 👸🏻🐶
                 </p>
                 <p>
 				2020년에 일본으로 넘어가 1년간 초등학교에서 외국어 교사 (FLT; Foreign Language Teacher)로 일을 했습니다. 그 후 의료 분야 일본 스타트업에 입사해 2년 동안 프론트엔드 개발자로 일을 했습니다. 사내 애플리케이션의 UI 수정 및 유지보수, 그리고 버그를 고치는 작업을 주로 했고, 회사 서비스 소개 및 문의 페이지를 혼자서 처음부터 만들기도 했습니다. 외국어 능력을 살려 개발자들 사이의 영어-일본어 통역도 했었습니다.
+                </p>
+                <hr />
+                <p>
+                Greetings, my name is Jii Eu and I have a hobby of playing Taiko drums (the actual taiko not the <a href='https://www.nintendo.com/store/products/taiko-no-tatsujin-drum-n-fun-switch/'>game</a>). Currently, I reside in a peaceful area of Kentucky with my partner and one furry companion 👸🏻🐶
+                </p>
+                <p>
+                In 2020, I worked as a Foreign Language Teacher (FLT) for one year at an elementary school in Japan. After that, I worked as a frontend developer for a healthcare startup in Japan for two years. My primary responsibilities included modifying and maintaining the company&apos;s internal application UI, debugging, and creating service introduction and inquiry pages from scratch. Additionally, I utilized my language skills to work as an English-Japanese interpreter among developers.
                 </p>
                 <BackButton className={'mx-0'} onClick={() => router.push('/')}>{'←'}</BackButton>
                 <ContactLinks>
